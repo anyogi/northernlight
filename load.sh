b09d03fea8aef14a2df374945f477a5cfb9f261f
anyogi <42035456+anyogi@users.noreply.github.com>
2018-08-24 09:20:42 +0200

    hostnameadded

--- load.sh
+++ load.sh
@@ -2,3 +2,5 @@ LOAD=`uptime | awk -F " " '{print $9}'`
 echo "Load on the server is $LOAD"
 NCPU=`lscpu | sed -n '4p'| awk -F " " '{print $2}'`
 echo " Number of CPU's in the server is/are $NCPU"
+NAME=`hostname`
+echo "Server hostname is $NAME"
